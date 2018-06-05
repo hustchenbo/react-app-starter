46ddd9c22f3793ff41b22f109a79fa4e5911543c
Chen Bo <chenbo09@baidu.com>
2018-06-05 16:54:30 +0800

    add dev proxy

---
+++ mockup/server.js
@@ -0,0 +1,32 @@
+/**
+ * @file mock服务入口
+ * @author chenbo09
+ */
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('mockup/db.json');
+
+const middlewares = jsonServer.defaults();
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+    // 因为本地mock不想改数据 先把所有的POST请求转为GET
+    if (req.method === 'POST') {
+        req.method = 'GET';
+    }
+    // Continue to JSON Server router
+    next();
+});
+
+// Use default router
+server.use(router);
+server.listen(3001, () => {
+    console.log('The JSON Server is listening on port 3000!');
+});
--- tools/server.js
+++ tools/server.js
@@ -7,40 +7,44 @@ const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
+const proxyMiddleware = require('http-proxy-middleware');
 const app = express();
 const config = require('./webpack.dev.js');
 const compiler = webpack(config);
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
-app.use(webpackDevMiddleware(compiler, {
-    publicPath: '/',
-    contentBase: './dist',
-    compress: true,
-    port: 3000,
-    hot: true,
-    lazy: false,
-    watchOptions: {
-        poll: true
-    }
-}));
+app.use(
+    webpackDevMiddleware(compiler, {
+        publicPath: '/',
+        contentBase: './dist',
+        compress: true,
+        port: 3000,
+        hot: true,
+        lazy: false,
+        watchOptions: {
+            poll: true
+        }
+    })
+);
 
+app.use(
+    proxyMiddleware('/api', {
+        // 转发到mockup的服务上了 具体见mockup/server
+        target: 'http://localhost:3001',
+        pathRewrite: {'^/api': ''}
+    })
+);
 
-app.use(webpackHotMiddleware(compiler, {
-    log: false,
-    path: '/__webpack_hmr',
-    heartbeat: 1000
-}));
+app.use(
+    webpackHotMiddleware(compiler, {
+        log: false,
+        path: '/__webpack_hmr',
+        heartbeat: 1000
+    })
+);
 
 // Serve the files on port 3000.
-app.listen(3000, function () {
+app.listen(3000, () => {
     console.log('The React app is listening on port 3000!\n');
 });
